d796def3bc24e61834d61666e71d58b078e8c217
Firagos <jemalfiragos@gmail.com>
2025-04-12 00:19:18 +0300

    i added getAllUser

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
-const protect = require("../middlewares/protect");
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
-
-router.use(protect);
+router.get("/", userController.protect, userController.getAllUser);
 
 router.get("/me", (req, res) => {
   res.status(200).json({
