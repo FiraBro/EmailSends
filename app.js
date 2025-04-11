5da0f46241cb00c0b1247714e2be8eac0bf465bb
Firagos <jemalfiragos@gmail.com>
2025-04-12 00:21:41 +0300

    i fix the bug

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const userRoutes = require("./routes/userRoutes");
+const userRoutes = require("./routes/userRoute");
 const emailRoutes = require("./routes/emailRoutes");
 
 mongoose.connect("mongodb://localhost:27017/mydatabase", {
