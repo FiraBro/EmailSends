e033e07ea98d876b76975adf9a722e7b5d521451
Firagos <jemalfiragos@gmail.com>
2025-04-12 00:20:39 +0300

    i change the service to gmail

--- utils/email.js
+++ utils/email.js
@@ -1,17 +1,30 @@
+// const nodemailer = require("nodemailer");
+
+// async function sendEmail(options) {
+//   const transporter = nodemailer.createTransport({
+//     host: "smtp.ethereal.email", // Replace with your SMTP host
+//     port: 587,
+//     secure: false, // true for port 465, false for other ports
+//     auth: {
+//       user: "jemalfiragos@gmail.com", // Replace with your email
+//       pass: "Ham3230@F", // Replace with your email password
+//     },
+//   });
+
+//   await transporter.sendMail(options);
+// }
+
+// module.exports = { sendEmail };
 const nodemailer = require("nodemailer");
 
 async function sendEmail(options) {
   const transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email", // Replace with your SMTP host
-    port: 587,
-    secure: false, // true for port 465, false for other ports
+    service: "gmail",
     auth: {
-      user: "your-email@example.com", // Replace with your email
-      pass: "your-email-password", // Replace with your email password
+      user: "jemalfiragos@gmail.com", // Your Gmail address
+      pass: "giesvnlouvsnquib", // Your Gmail app password
     },
   });
-
   await transporter.sendMail(options);
 }
-
 module.exports = { sendEmail };
