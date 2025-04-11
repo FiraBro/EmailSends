fde38cc0ecded5d666ecb2c63f99ec6e289eb744
Firagos <jemalfiragos@gmail.com>
2025-04-11 18:14:27 +0300

    i recreate app.js file

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require("express");
-const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/userRoutes");
+const emailRoutes = require("./routes/emailRoutes");
+
+mongoose.connect("mongodb://localhost:27017/mydatabase", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 const app = express();
-const morgan = require("morgan");
-const dotenv = require("dotenv");
-app.use(cors());
-// Load environment variables
-dotenv.config({ path: "config.env" });
 
-// Middleware
-app.use(express.json());
-app.use(morgan("dev"));
+app.use(bodyParser.json());
 
-// Routes
-const userRouter = require("./routes/userRouter");
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/emails", emailRoutes);
 
-app.use("/api/v1/users", userRouter);
-
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
