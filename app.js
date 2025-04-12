0a3782ca9a1a5006d824e73768ab4285e1922df3
Firagos <jemalfiragos@gmail.com>
2025-04-12 23:55:03 +0300

    i modify the app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/userRoute");
 const emailRoutes = require("./routes/emailRoutes");
+const courseRoutes = require("./routes/courseRouter");
 mongoose.connect("mongodb://localhost:27017/mydatabase", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -11,6 +12,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/emails", emailRoutes);
+app.use("/api/v1/courses", courseRoutes);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
